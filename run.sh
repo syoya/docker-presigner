cb70683419af1d41b0ce38268c7c5eef3b187503
minodisk <daisuke.mino@gmail.com>
2017-08-30 15:24:28 +0900

    Debug

--- run.sh
+++ run.sh
@@ -5,6 +5,12 @@ set -e
 mkdir /secret
 printf "%s" "$GOOGLE_AUTH_JSON" > /secret/google-auth.json
 
+echo "------------------"
+echo $BUCKET
+echo $PREFIX
+echo $PORT
+echo "------------------"
+
 presigner \
   -account /secret/google-auth.json \
   -bucket $BUCKET \
